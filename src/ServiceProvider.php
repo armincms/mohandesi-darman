d1bec4cbd0bc2e3fc61c749361e860b3702fd031
اسماعیل زارع <zarehesmaiel@gmail.com>
2022-01-01 18:13:46 +0330

    First commit

---
+++ src/ServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Armincms\MDarman;
+
+use Illuminate\Contracts\Support\DeferrableProvider;
+use Illuminate\Support\ServiceProvider as LaravelServiceProvider; 
+
+class ServiceProvider extends LaravelServiceProvider implements DeferrableProvider
+{ 
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    { 
+         
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
+
+    /**
+     * Get the events that trigger this service provider to register.
+     *
+     * @return array
+     */
+    public function when()
+    {
+        return [
+            \Core\HttpSite\Events\ServingFront::class,
+            \Illuminate\Console\Events\ArtisanStarting::class,
+        ];
+    }
+}
